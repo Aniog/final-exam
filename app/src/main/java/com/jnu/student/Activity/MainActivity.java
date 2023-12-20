7e7b0019fef1ba9d69a7e02c12f27f8bc0376f55
Aniog <3158196725@qq.com>
2023-12-20 20:56:38 +0800

    实现添加功能中的新建任务，加入副本和排序功能

--- app/src/main/java/com/jnu/student/Activity/MainActivity.java
+++ app/src/main/java/com/jnu/student/Activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.jnu.student.Activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +19,12 @@ import com.jnu.student.R;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView btmNavView;
+    // 声明一个用于启动带有返回结果的活动的管理器(添加任务)
+    ActivityResultLauncher<Intent> addTasksLauncher;
+    // 声明一个用于启动带有返回结果的活动的管理器(加入副本)
+    ActivityResultLauncher<Intent> addDungeonLauncher;
+    // 声明一个用于启动带有返回结果的活动的管理器(排序)
+    ActivityResultLauncher<Intent> SortLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +35,41 @@ public class MainActivity extends AppCompatActivity {
             Fragment tasksFragment = new FirstTasksFragment();
             loadTasksFragment(tasksFragment);
         }
+        // 为启动带有返回结果的活动(Activity)注册一个处理程序(添加任务)
+        addTasksLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
 
+                    } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+
+                    }
+                }
+        );
+        // 为启动带有返回结果的活动(Activity)注册一个处理程序(添加副本)
+        addDungeonLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+
+                    } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+
+                    }
+                }
+        );
+        // 为启动带有返回结果的活动(Activity)注册一个处理程序(排序)
+        SortLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+
+                    } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+
+                    }
+                }
+        );
         // 获取底部导航栏
-        btmNavView = findViewById(R.id.bottom_navigation_menu);
+        btmNavView = findViewById(R.id.bottom_menu);
         btmNavView.setOnItemSelectedListener(item -> {
             // 处理底部导航栏项的选择
             if (item.getItemId() == R.id.navigation_rengwu) {
@@ -37,14 +79,16 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
             if (item.getItemId() == R.id.navigation_jiangli) {
-                // 用户点击了“奖励”，这里可以添加对应逻辑
+                // 用户点击了“首页”
                 return true;
             }
             if (item.getItemId() == R.id.navigation_tongji) {
+                // 用户点击了“统计”，这里可以添加对应逻辑
+
                 return true;
             }
             if (item.getItemId() == R.id.navigation_wo) {
-
+                // 用户点击了“奖励”，加载DailyTasksFragment
                 return true;
             }
             return false;
@@ -52,14 +96,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // 加载Fragment的方法
-
-
     private void loadTasksFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit();
     }
-
     @Override
     // 创建选项菜单
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,9 +111,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == R.id.btn_msg)
-        {
+    @Override
+    //点击添加按钮
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.btn_msg) {
             // 创建一个 PopupMenu
             PopupMenu popupMenu = new PopupMenu(this, findViewById(R.id.btn_msg));
             // 在菜单中添加选项
@@ -85,15 +127,21 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getTitle().toString()) {
                     case "新建任务":
                         // 处理选项一点击事件
-                        Toast.makeText(this, "新建任务", Toast.LENGTH_SHORT).show();
+                        // 创建一个新的意图（Intent）以启动 AddTasksActivity
+                        Intent intent1 = new Intent(this, AddTasksActivity.class);
+                        // 使用 addTasksLauncher 启动指定的 Intent
+                        addTasksLauncher.launch(intent1);
                         return true;
                     case "加入副本":
                         // 处理选项二点击事件
-                        Toast.makeText(this, "加入副本任务", Toast.LENGTH_SHORT).show();
+                        // 创建一个新的意图（Intent）以启动 AddTasksActivity
+                        Intent intent2 = new Intent(this, addFubengActivity.class);
+                        // 使用 addTasksLauncher 启动指定的 Intent
+                        addTasksLauncher.launch(intent2);
                         return true;
                     case "排序":
                         // 处理选项三点击事件
-                        Toast.makeText(this, "排序", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "排序成功", Toast.LENGTH_SHORT).show();
                         return true;
                 }
                 return false;
--- app/src/main/java/com/jnu/student/Activity/addFubengActivity.java
+++ app/src/main/java/com/jnu/student/Activity/addFubengActivity.java
@@ -4,15 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.jnu.student.R;
 
-public class BottomNavegationActivity extends AppCompatActivity {
-    private BottomNavigationView btmNavView;
+public class addFubengActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.first_tasks);
-
+        setContentView(R.layout.add_fubeng);
     }
 }
